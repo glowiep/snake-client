a7fd36ed8cb23af05e495fae5a38ebb1be527fe2
Gloria Lim <glorialimartt@gmail.com>
2023-10-14 20:28:09 -0400

    Separate module for the TCP connection

--- play.js
+++ play.js
@@ -1,21 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  });
-
-  conn.on("data", () => {
-    console.log("you ded cuz you idled");
-  })
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const {connect} = require("./client");
 
 console.log("Connecting ...");
 connect();
