379d820a2356e5b1b43ca96558ef35cd69d1b826
Gloria Lim <glorialimartt@gmail.com>
2023-10-19 16:28:19 -0400

    Linted file

--- constants.js
+++ constants.js
@@ -2,15 +2,15 @@ const IP = "localhost";
 const PORT = 50541;
 
 const CONNECT_MSG = "Successfully connected to game server";
-const EXIT_MSG = "you ded cuz you idled"
+const EXIT_MSG = "you ded cuz you idled";
 
-const USERNAME = "GL"
+const USERNAME = "GL";
 
 const CANNED_MSG = {
   m: "Say: How are you?",
   "," : "Say: I'm winning!",
   n: "Say: You're winning!"
-}
+};
 
 const MOVE_KEY = {
   w: "Move: up",
@@ -21,7 +21,7 @@ const MOVE_KEY = {
   '\u001B\u005B\u0044': "Move: left",
   '\u001B\u005B\u0042': "Move: down",
   '\u001B\u005B\u0043':"Move: right"
-}
+};
 
 module.exports = {
   IP,
